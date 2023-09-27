18c819b99aa75ea42f49c45069497150cad5d054
Amokstakov <amokstakov@gmail.com>
2023-09-26 22:49:54 -0700

    Initial commit

---
+++ test/rag_stack_fargate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RagStackFargate from '../lib/rag_stack_fargate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rag_stack_fargate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RagStackFargate.RagStackFargateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
